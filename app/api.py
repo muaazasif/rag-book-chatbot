1d2ee0d53f93065b910f66acb9d6fba927b7a449
muaazasif <muhammed.muaaz@gmail.com>
2025-12-28 22:37:27 +0500

    change model embeddings.py

--- app/api.py
+++ app/api.py
@@ -55,4 +55,8 @@ def ask_question(req: QuestionRequest):
     return {"question": req.question, "answer": answer}
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+
+    port = int(os.environ.get("PORT", 8000))  # Railway automatically sets $PORT
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
